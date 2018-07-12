33eb85dd4b1eb239fb2fa0a75398249f1cbcc517
kalamanadeygit <kalamanadey@gmail.com>
2018-07-12 11:03:53 +0300

    derniere modification

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,12 @@ $(document).ready(function() {
        $(".divhid").toggle();
      });
    });
+   /* Open the sidenav */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
